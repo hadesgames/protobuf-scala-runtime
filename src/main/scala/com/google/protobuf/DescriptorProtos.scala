2e4f0b8be1b375ca974d497d84f37b0c1662fbf2
Alexandru Tache <hadesgames@gmail.com>
2016-07-04 11:20:34 +0100

    Added more stubs for ScalaJS

--- src/main/scala/com/google/protobuf/DescriptorProtos.scala
+++ src/main/scala/com/google/protobuf/DescriptorProtos.scala
@@ -3,8 +3,14 @@ package com.google.protobuf
 object DescriptorProtos {
   class FileDescriptorProto
 
+  def getDescriptor(): Descriptors.FileDescriptor = ???
   object FileDescriptorProto {
     def parseFrom(b: Array[Byte]): FileDescriptorProto = ???
   }
 }
 
+object UnknownFieldSet {
+  class Field
+}
+
+class Message
